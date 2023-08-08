4459963ea3dec7c8adf43453ff7fb360137358d4
samilkafa <samilkafa@gmail.com>
2023-08-08 13:38:44 +0300

    Hafta-13-Challenge

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,34 @@
 // projects ara yazılımları buraya
+const projectModel = require("./projects-model");
+
+const validateProjectId = async (req, res, next) => {
+    try {
+        const existProject = await projectModel.get(req.params.id);
+        if (!existProject) {
+            res.status(404).json({ message: "Project not found" });
+        } else {
+            req.existProject = existProject;
+            next();
+        }
+    } catch (error) {
+        next(error);
+    }
+};
+
+const validateProjectPayload = (req, res, next) => {
+    try {
+        const { name, description } = req.body
+        if(!name || !description) {
+            res.status(400).json({message: "Check fields"})
+        } else {
+            next();
+        }
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = {
+    validateProjectId,
+    validateProjectPayload
+}
